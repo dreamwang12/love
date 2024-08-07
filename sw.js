531f8eeba430e1b62b25285ff1f64f72be18cb7a
dreamwang12 <184079591@qq.com>
2024-08-07 16:21:44 +0800

    Site updated: 2024-08-07 16:21:44

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723016337754";
+const VERSION = "1723018863083";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
